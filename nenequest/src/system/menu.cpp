f627d66aaea1fbdf85db20733214377192c39954
engboris <boris.eng@ocamlpro.com>
2024-07-13 17:15:40 +0200

    Fix mouse position on menu and refactoring

--- nenequest/src/system/menu.cpp
+++ nenequest/src/system/menu.cpp
@@ -5,99 +5,112 @@ using namespace std;
 using namespace sf;
 
 Menu::Menu() { }
-
 Menu::~Menu() { }
+
+class ActionPair {
+  public:
+    ActionPair(Text* t, ScreenState s) {
+      button = t;
+      screen = s;
+    };
+    Text* button;
+    ScreenState screen;
+};
 
 ScreenState Menu::run(RenderWindow& app) {
     Event event;
     int menuIndex = 0;
-    ScreenState selectedByMouse = GAME_SCREEN;
+    ScreenState selectedScreen = GAME_SCREEN;
 
     // ---------------- Logo ----------------
     Texture logoTex;
     logoTex.loadFromFile("img/titlescreen.png");
     logoTex.setSmooth(true);
     Sprite logo(logoTex);
-    logo.setOrigin(Vector2f(logoTex.getSize().x/2, logoTex.getSize().y/2));
-    logo.setPosition(app.getSize().x/2, app.getSize().y/2);
+		FloatRect logolb = logo.getLocalBounds();
+    logo.setOrigin(logolb.width/2, logolb.height/2);
+    logo.setPosition(app.getSize().x/2, app.getSize().y/2 - 200);
     logo.setScale(1.45, 1.45);
-    logo.move(0, -200);
 
     // ---------------- Buttons ----------------
     Font font;
     font.loadFromFile("fonts/JapanSans100.ttf");
-
     Text p1button(L"\u3072\u3068\u308A\u3067\u3042\u305D\u3076", font);
     Text p2button(L"\u3075\u305F\u308A\u3067\u3042\u305D\u3076", font);
     Text exitButton(L"\u304A\u308F\u308B", font);
 
-    array<pair<Text*, ScreenState>, 3> buttons = {
-        make_pair(&p1button, GAME_SCREEN),
-        make_pair(&p2button, GAME_OVER), // TODO : temporary, for tests
-        make_pair(&exitButton, EXIT_GAME)
+    array<ActionPair, 3> actionPairs = {
+        ActionPair(&p1button, GAME_SCREEN),
+        ActionPair(&p2button, GAME_OVER), // TODO : temporary, for tests
+        ActionPair(&exitButton, EXIT_GAME)
     };
 
-    for (unsigned int i=0; i<buttons.size(); i++) {
-        Text* t = buttons[i].first;
+    for (unsigned int i=0; i<actionPairs.size(); i++) {
+        Text* t = actionPairs[i].button;
         t->setCharacterSize(this->TEXT_SIZE);
-        t->setPosition(logo.getPosition());
-        t->move(-300, 300 + i * 100);
+        t->move(
+            logo.getPosition().x - 300,
+            logo.getPosition().y + 300 + i * 100
+        );
         t->setFillColor(Color(101, 45, 86, 255));
         t->setStyle(Text::Bold);
     }
 
-    // ---------------- Selector ----------------
-    Texture selectorTex;
-    selectorTex.loadFromFile("img/cursor.png");
-    selectorTex.setSmooth(true);
+    // ---------------- Cursor ----------------
+    Texture cursorTex;
+    cursorTex.loadFromFile("img/cursor.png");
+    cursorTex.setSmooth(true);
 
-    Sprite selector(selectorTex);
-    selector.setOrigin(Vector2f(selectorTex.getSize().x, 0));
-    selector.setScale(0.3, 0.3);
-    selector.setPosition(p1button.getPosition());
+    Sprite cursor(cursorTex);
+    cursor.setOrigin(Vector2f(cursorTex.getSize().x, 0));
+    cursor.setScale(0.3, 0.3);
+    cursor.setPosition(p1button.getPosition());
 
     // ---------------- Main Loop ----------------
     while (true) {
         Vector2i mousePos = Mouse::getPosition(app);
 
         while (app.pollEvent(event)) {
-            if (event.type == Event::Closed) return EXIT_GAME;
+            if (event.type == Event::Closed)
+                return EXIT_GAME;
 
             if (event.type == Event::KeyPressed) {
                 switch (event.key.code) {
-                    case Keyboard::Up: menuIndex = (menuIndex-1)%buttons.size(); break;
-                    case Keyboard::Down: menuIndex = (menuIndex+1)%buttons.size(); break;
-                    case Keyboard::Return: return buttons[menuIndex].second; break;
-                    default: break;
+                    case Keyboard::Up:
+                      menuIndex = (menuIndex-1)%actionPairs.size();
+                      break;
+                    case Keyboard::Down:
+                      menuIndex = (menuIndex+1)%actionPairs.size();
+                      break;
+                    case Keyboard::Return:
+                      return actionPairs[menuIndex].screen;
+                      break;
+                    default:
+                      break;
                 }
-                selector.setPosition(buttons[menuIndex].first->getPosition());
+                cursor.setPosition(actionPairs[menuIndex].button->getPosition());
             }
 
             if (event.type == Event::MouseMoved) {
-                for (auto b : buttons) {
-                    if (isSelected(b.first, mousePos.y)) {
-                        selector.setPosition(b.first->getPosition());
-                        selectedByMouse = b.second;
+                for (auto action : actionPairs) {
+                    if (action.button->getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                        cursor.setPosition(action.button->getPosition());
+                        selectedScreen = action.screen;
                     }
                 }
             }
 
             if (event.type == Event::MouseButtonPressed)
-                return selectedByMouse;
+                return selectedScreen;
         }
 
         app.clear(Color::White);
         app.draw(logo);
-        for (auto b : buttons)
-            app.draw(*b.first);
-        app.draw(selector);
+        for (auto action : actionPairs)
+            app.draw(*action.button);
+        app.draw(cursor);
         app.display();
     }
 
     return EXIT_GAME;
-}
-
-bool Menu::isSelected(Text* button, int mouseY) {
-    return button->getGlobalBounds().top >= (float)mouseY && 
-    button->getGlobalBounds().top-button->getGlobalBounds().height <= (float)mouseY;
 }
